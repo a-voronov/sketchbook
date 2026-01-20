27f8051cdaf56319f3257eb3f796b2726edda3ef
Oleksandr Voronov <voronovaleksandr91@gmail.com>
2026-01-20 02:02:42 +0200

    refactored binary tree

--- openFrameworks/apps/myApps/mazes/src/binaryTree.cpp
+++ openFrameworks/apps/myApps/mazes/src/binaryTree.cpp
@@ -2,17 +2,12 @@
 
 void BinaryTree::on(Grid& grid, std::mt19937& rng) {
     grid.each_cell([&rng](Cell& cell) {
-        vector<Cell*> neighbors;
+        std::uniform_int_distribution<int> dist(0, 1);
 
-        if (cell.north != nullptr) neighbors.push_back(cell.north);
-        if (cell.east  != nullptr) neighbors.push_back(cell.east);
+        Cell *neighbor = dist(rng) == 0
+            ? (cell.north ?: cell.east)
+            : (cell.east ?: cell.north);
 
-        if (neighbors.empty()) return;
-
-        std::uniform_int_distribution<int> dist(0, neighbors.size() - 1);
-        int idx = dist(rng);
-        auto neighbor = neighbors.at(idx);
-
-        if (neighbor != nullptr) cell.link(neighbor);
+        cell.link(neighbor);
     });
 }
