4701c5c9b60bd2bd0a9c4ae7a0a92495722ac972
Oleksandr Voronov <voronovaleksandr91@gmail.com>
2026-02-01 23:59:53 +0200

    implemented color-based grid

---
+++ openFrameworks/apps/myApps/mazes/src/colorGrid.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "distances.h"
+#include "grid.h"
+#include "mazeUtils.h"
+
+using namespace std;
+
+struct ColorGrid : Grid {
+public:
+    ColorGrid(int rows, int columns)
+        : Grid(rows, columns) {}
+    ColorGrid()
+        : ColorGrid(0, 0) {}
+
+    void set_distances(const Distances& distances) {
+        distances_ = make_unique<Distances>(distances);
+        maximum_   = distances.max().second;
+    }
+
+    ofColor background_color_for(const Cell& cell, const ColorCfg& color_cfg) const override {
+        if (!distances_) return ofColor::white;
+        int distance = distances_->get(cell);
+        if (distance < 0) return ofColor::white;
+
+        float intensity = 1.0f - ((maximum_ - distance) / maximum_);
+
+        return color_intensity(color_cfg.tone, intensity * std::clamp(color_cfg.intensity_stretch, 0.0f, 1.0f));
+    }
+
+private:
+    unique_ptr<Distances> distances_ = nullptr;
+    float maximum_;
+};
